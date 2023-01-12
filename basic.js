0a52b1ca9cfa6f5f6a56dffc8273cc7b03aca68f
fatematuzjohora <fatematuzjohora889@gmail.com>
2023-01-12 13:48:55 +0600

    while loop added

--- basic.js
+++ basic.js
@@ -1,39 +1,44 @@
-let greeting = "Hello, My name is";
-let myname = "Fatema";
-let lastname = "tuzJohora";
-let fullname = myname + " " + lastname;
+// let greeting = "Hello, My name is";
+// let myname = "Fatema";
+// let lastname = "tuzJohora";
+// let fullname = myname + " " + lastname;
 
 
-let str = "And I have";
-let apple = 50;
-apple *= 3;
-let end = "apples";
-console.log(greeting, fullname, str, apple, end);
-let a = 4+7;
-console.log(a);
-let b = 5*9;
-console.log(b);
-let c = b/a;
-console.log(c);
-let x = 32;
-if(x == 9){
-  console.log('true');
-}else{
-  console.log('false');
-}
+// let str = "And I have";
+// let apple = 50;
+// apple *= 3;
+// let end = "apples";
+// console.log(greeting, fullname, str, apple, end);
+// let a = 4+7;
+// console.log(a);
+// let b = 5*9;
+// console.log(b);
+// let c = b/a;
+// console.log(c);
+// let x = 32;
+// if(x == 9){
+//   console.log('true');
+// }else{
+//   console.log('false');
+// }
 
-let y = 'red';
-if(y == "blue"){
-  console.log('blue is detected')
-}else if(y == "green"){
-  console.log('green is detceted')
-}else{
-  console.log('not blue not green')
-}
-let n = 64;
-if(n>45){
-  if(n<65){
-    console.log(n);
-  }
+// let y = 'red';
+// if(y == "blue"){
+//   console.log('blue is detected')
+// }else if(y == "green"){
+//   console.log('green is detceted')
+// }else{
+//   console.log('not blue not green')
+// }
+// let n = 64;
+// if(n>45){
+//   if(n<65){
+//     console.log(n);
+//   }
+// }
+let i = 2;
+while(i<10){
+    console.log(i);
+    i++;
 }
 
