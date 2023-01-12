ab27aa733235c1a44d827c38382a5d1c889eb9bb
fatematuzjohora <fatematuzjohora889@gmail.com>
2023-01-12 18:06:24 +0600

    eqality

--- basic.js
+++ basic.js
@@ -36,9 +36,67 @@
 //     console.log(n);
 //   }
 // }
-let i = 2;
-while(i<10){
+// let i = 2;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+for(let i = 7; i < 13; i++) {
     console.log(i);
-    i++;
 }
 
+let num = [8, 9, 10, 11, 12, 13, 14, 15];
+console.log(num[3]);
+console.log(num);
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let len = arr.length;
+// for(let i = 0; i < len; i++){
+//     if(arr[i] > 4){
+//         console.log(arr[i]);
+//     }
+// }
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let len = arr.length;
+for(let i = 0; i < len; i++){
+    if(arr[i] > 4){
+        console.log(arr[i]);
+    }
+}
+function welcomeToBooleans() {
+    return true;}
+    function trueOrFalse(wasThatTrue) {
+        if(wasThatTrue){
+          return "Yes, that was true";
+        }
+       return "No, that was false"
+       }
+       function testEqual(val) {
+        if (val == 12) { 
+          return "Equal";
+        }
+        return "Not Equal";
+      }
+      
+      testEqual(10);
+      function testNotEqual(val) {
+        if (val != 99) { 
+          return "Not Equal";
+        }
+        return "Equal";
+      }
+      
+      testNotEqual(10);
+      function testGreaterOrEqual(val) {
+        if (val >= 20) {  
+          return "20 or Over";
+        }
+      
+        if (val >= 10) {  
+          return "10 or Over";
+        }
+      
+        return "Less than 10";
+      }
+      
+      testGreaterOrEqual(10);
