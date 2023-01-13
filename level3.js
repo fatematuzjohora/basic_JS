1194022f42c5cd91a4375406f3bd0d1826cd1a85
fatematuzjohora <fatematuzjohora889@gmail.com>
2023-01-13 17:30:13 +0600

    new update

--- level3.js
+++ level3.js
@@ -9,11 +9,20 @@
 // }
 // console.log(arr);
 
-let num = [3,10,9,5,2,11,7,15,12,4,1,13,6,14,8];
-for(let i=0; i < num.length; i++){
-   let v = num[i];
-   if(v <=7 && v >=3){
-    console.log(num[i]);
+// let num = [3,10,9,5,2,11,7,15,12,4,1,13,6,14,8];
+// for(let i=0; i < num.length; i++){
+//    let v = num[i];
+//    if(v <=7 && v >=3){
+//     console.log(num[i]);
+//    }
+
+// }
+
+let count = [3,10,9,5,2,11,7,15,12,4,1,13,6,14,8];
+for(let i=0; i < count.length; i++){
+   let v = count[i];
+   if(v >=1 && v < 3 || v >=8 && v <= 15){
+    console.log(count[i]);
    }
 
 }
