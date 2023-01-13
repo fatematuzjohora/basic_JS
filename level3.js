d604974ab48bb122f068a204603b626054cae81a
fatematuzjohora <fatematuzjohora889@gmail.com>
2023-01-13 17:05:02 +0600

    new update

--- level3.js
+++ level3.js
@@ -1,10 +1,19 @@
-for(let i = 102; i >= 27; i -=3){
-    console.log(i);
-}
+// for(let i = 102; i >= 27; i -=3){
+//     console.log(i);
+// }
 
-let arr = [];
-for(let i = 3; i <13; i++){
-    arr.push(i);
+// let arr = [];
+// for(let i = 3; i <13; i++){
+//     arr.push(i);
+
+// }
+// console.log(arr);
+
+let num = [3,10,9,5,2,11,7,15,12,4,1,13,6,14,8];
+for(let i=0; i < num.length; i++){
+   let v = num[i];
+   if(v <=7 && v >=3){
+    console.log(num[i]);
+   }
 
 }
-console.log(arr);
